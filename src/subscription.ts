af4ab15d73b611eb63737336de7804738ce748c8
Yu MORIYA <euro21st@gmail.com>
2024-02-24 00:17:36 +0900

    Remove multi word logic

--- src/subscription.ts
+++ src/subscription.ts
@@ -6,9 +6,9 @@ import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 import * as fs from 'fs';
 
 const targetsOneWord = JSON.parse(fs.readFileSync('./static/targetsOneWord.json', 'utf-8'));
-const targetsMultiWord = JSON.parse(fs.readFileSync('./static/targetsMultiWord.json', 'utf-8'));
-const ngWords = JSON.parse(fs.readFileSync('./static/ngWords.json', 'utf-8'));
-const targetWord = "阪神";
+// const targetsMultiWord = JSON.parse(fs.readFileSync('./static/targetsMultiWord.json', 'utf-8'));
+// const ngWords = JSON.parse(fs.readFileSync('./static/ngWords.json', 'utf-8'));
+// const targetWord = "阪神";
 
 function includesAny(text: string, targetsOneWord: string[]): boolean {
   return targetsOneWord.some((target) => text.includes(target));
@@ -28,9 +28,10 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToCreate = ops.posts.creates
       .filter((create) => {
         // only tigers-related posts
-        if (includesAny(create.record.text, ngWords)) return false;
-        return includesAny(create.record.text, targetsOneWord) ||
-          includesMulti(create.record.text, targetsMultiWord, targetWord)
+        // if (includesAny(create.record.text, ngWords)) return false;
+        // return includesAny(create.record.text, targetsOneWord) ||
+        //   includesMulti(create.record.text, targetsMultiWord, targetWord)
+        return includesAny(create.record.text, targetsOneWord)
       })
       .map((create) => {
         // map alf-related posts to a db row
